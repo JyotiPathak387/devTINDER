36319b45912f795b655b80df0ea7fe3f928b7336
unknown <jyotipathak387@gmail.com>
2024-11-27 10:43:58 +0530

    learning route

--- src/app.js
+++ src/app.js
@@ -2,21 +2,31 @@ const express = require("express");
 
 const app = express();
 
-
-app.use("/hello", (req, res ) => {
-    res.send("hello hello hello jyoti pathak");
-   })
-
- 
-   
-app.use("/test", (req, res ) => {
- res.send("hello from the server");
-})
+//made the route dynamic dynamic
+// app.get("/user/:userId/:name/:password", (req, res ) => {
+//     console.log(req.params)
+//     res.send({firstname : "Jyoti", lastname : "Pathak"})
+//    })
 
 
-app.use("/", (req, res ) => {
-    res.send("hello");
-   })
+// app.get("/user", (req, res ) => {
+//     console.log(req.query)
+//     res.send({firstname : "Jyoti", lastname : "Pathak"})
+//    })
+
+ app.get("/user/*fly$", (req, res ) => {
+        res.send({firstname : "Jyoti", lastname : "Pathak"})
+       })
+
+// app.post("/user", (req, res ) => {
+//     res.send("hello from post method");
+//    })
+
+// app.delete("/user", (req, res 
+// ) => {
+//     res.send("hello from delete method");
+//    })
+
 
 app.listen(7777, () => {
     console.log("listen port 7777");
